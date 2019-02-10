65dfa56cd02df57a01576b01ae2920c6fcbb5b45
Rafid Ahsan <rafidahsan698@gmail.com>
2019-02-11 01:03:17 +0600

    shitt

---
+++ js/chart.js
@@ -0,0 +1,74 @@
+window.onload = function () {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        exportEnabled: true,
+        animationEnabled: true,
+        title:{
+            text: "Your Statistics"
+        },
+        axisX: {
+            title: "Numbers"
+        },
+        axisY: {
+            title: "Good marks",
+            titleFontColor: "#4F81BC",
+            lineColor: "#4F81BC",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC",
+            valueFormatString: "#0"
+        },
+        axisY2: {
+            title: "Bad Marks",
+            titleFontColor: "#C0504E",
+            lineColor: "#C0504E",
+            labelFontColor: "#C0504E",
+            tickColor: "#C0504E",
+            valueFormatString: "#0"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "column",
+            name: "Right Answer",
+            showInLegend: true,      
+            yValueFormatString: "",
+            dataPoints: [
+                { label: "Physics",  y: 10.5 },
+                { label: "Math", y: 50 },
+                { label: "Chemestry", y: 55 },
+                { label: "Biology",  y: 78 },
+                { label: "Zoology",  y: 80 }
+            ]
+        },
+        {
+            type: "column",
+            name: "Wrong Answer",
+            axisYType: "secondary",
+            showInLegend: true,
+            yValueFormatString: "",
+            dataPoints: [
+                { label: "Physics",  y: 5.5 },
+                { label: "Math", y: 80 },
+                { label: "Chemestry", y: 100 },
+                { label: "Biology",  y: 23 },
+                { label: "Zoology",  y: 87 }
+            ]
+        }]
+    });
+    chart.render();
+    
+    function toggleDataSeries(e) {
+        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        e.chart.render();
+    }
+    
+    }
