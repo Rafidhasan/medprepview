e466304bba89df0374e146c35f56c5dd063f45f3
Rafid Ahsan <rafidahsan698@gmail.com>
2019-02-25 02:58:42 +0600

    add last caousel on exam page

--- question/learning/js/chart.js
+++ question/learning/js/chart.js
@@ -1,18 +1,18 @@
-window.onload = function() {
-
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        data: [{
-            type: "pie",
-            startAngle: 240,
-            yValueFormatString: "##0.00\"%\"",
-            indexLabel: "{label} {y}",
-            dataPoints: [
-                {y: 45, label: "Correct"},
-                {y: 20, label: "Incorrrect"},
-                {y: 35, label: "Partially correct"}
-            ]
-        }]
-    });
-    chart.render();
-}
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Correct", "Incorrect"],
+      datasets: [
+        {
+          label: "Answer",
+          backgroundColor: ["#228B22", "#f00"],
+          data: [30, 70]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+      }
+    }
+  });
