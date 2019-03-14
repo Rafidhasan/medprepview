024a767c5f140f0d673ac1c2ad6c45159f330d3e
Rafid Ahsan <rafidahsan698@gmail.com>
2019-03-14 15:18:36 +0600

    change the admin panel

--- js/chart.js
+++ js/chart.js
@@ -1,75 +1,53 @@
-window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer", {
-        exportEnabled: true,
-        animationEnabled: true,
-        title:{
-            text: "Your Statistics"
+var options = {
+    chart: {
+        height: 250,
+        type: 'bar',
+        stacked: true,
+        toolbar: {
+            show: true
         },
-        axisX: {
-            title: "Numbers"
-        },
-        axisY: {
-            title: "Good marks",
-            titleFontColor: "#008080",
-            lineColor: "#008080",
-            labelFontColor: "#4F81BC",
-            tickColor: "#4F81BC",
-            valueFormatString: "#0"
-        },
-        axisY2: {
-            title: "Bad Marks",
-            titleFontColor: "#C0504E",
-            lineColor: "#C0504E",
-            labelFontColor: "#C0504E",
-            tickColor: "#C0504E",
-            valueFormatString: "#0"
-        },
-        toolTip: {
-            shared: true
-        },
-        legend: {
-            cursor: "pointer",
-            itemclick: toggleDataSeries
-        },
-        data: [{
-            type: "column",
-            color: "#008080",  
-            name: "Right Answer",
-            showInLegend: true,      
-            yValueFormatString: "",
-            dataPoints: [
-                { label: "Physics",  y: 10.5 },
-                { label: "Math", y: 50 },
-                { label: "Chemestry", y: 55 },
-                { label: "Biology",  y: 78 },
-                { label: "Zoology",  y: 80 }
-            ]
-        },
-        {
-            type: "column",
-            color: "#FF4500",  
-            name: "Wrong Answer",
-            axisYType: "secondary",
-            showInLegend: true,
-            yValueFormatString: "",
-            dataPoints: [
-                { label: "Physics",  y: 5.5 },
-                { label: "Math", y: 80 },
-                { label: "Chemestry", y: 100 },
-                { label: "Biology",  y: 23 },
-                { label: "Zoology",  y: 87 }
-            ]
-        }]
-    });
-    chart.render();
-    
-    function toggleDataSeries(e) {
-        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        } else {
-            e.dataSeries.visible = true;
+        zoom: {
+            enabled: true
         }
-        e.chart.render();
-    }
-    
-    }
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+            }
+        }
+    }],
+    plotOptions: {
+        bar: {
+            horizontal: false,
+        },
+    },
+    series: [{
+        name: 'PRODUCT A',
+        data: [44, 55, 41, 67, 82, 43]
+    },{
+        name: 'PRODUCT B',
+        data: [13, 23, 20, 8, 13, 27]
+    }],
+    xaxis: {
+        type: 'datetime',
+        categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT', '01/05/2011 GMT', '01/06/2011 GMT'],
+    },
+    legend: {
+        position: 'right',
+        offsetY: 40
+    },
+    fill: {
+        opacity: 1
+    },
+}
+
+var chart = new ApexCharts(
+    document.querySelector("#chart"),
+    options
+);
+
+chart.render();
