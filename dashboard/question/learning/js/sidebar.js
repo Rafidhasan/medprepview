a779c7e4ef64718f8ef772c654ee9fcd8c821a70
md_Ahsan <rafidahsan698@gmail.com>
2019-03-20 20:33:56 +0600

    fix learning page

--- dashboard/question/learning/js/sidebar.js
+++ dashboard/question/learning/js/sidebar.js
@@ -1,2 +1,8 @@
-$('.ui.sidebar').sidebar({
-}).sidebar('attach events', '#mobile_item');
+$(".openbtn").on("click", function() {
+  $(".ui.sidebar").toggleClass("very thin icon");
+  $(".custom").toggleClass("marginlefting");
+  $(".sidebar z").toggleClass("displayblock");
+  $(".ui.accordion").toggleClass("displayblock");
+  $(".ui.dropdown.item").toggleClass("displaynone");
+  $(".brand-logo").toggleClass("displaynone");
+});
